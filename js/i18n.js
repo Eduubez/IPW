92c628dd38f7b7723d39edfc58d1dd39d19b0ff1
Eduubez <147525681+Eduubez@users.noreply.github.com>
2026-03-22 18:13:53 +0000

    Removed unsed Folder "IPW"

--- js/i18n.js
+++ js/i18n.js
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import translastionPT from "../IPW/resources/translation/pt/translation.json";
+import translastionPT from "./resources/translation/pt/translation.json";
 
 // the translations
 // (tip move them in a JSON file and import them,
