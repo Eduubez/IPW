b4acb320ba36b1beb428cb943cce321db265c7d3
Eduubez <147525681+Eduubez@users.noreply.github.com>
2026-04-19 00:02:40 +0100

    feat: added gradlew dependenci to repository-jdbi

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -5,7 +5,7 @@ import SideBar from "./Components/SideBar/SideBar";
 import Dashboard from "./Pages/Dashboard/DashBoard";
 import Profile from "./Pages/Profile/Profile";
 import RoleSelection from "./Pages/RoleSelection/RoleSelection";
-import  NewProcess  from "./Pages/NewProcess/NewProcess";
+import NewProcess from "./Pages/NewProcess/NewProcess";
 import { ProtectedRoute } from "./Components/ProtectedRoute/ProtectedRoute";
 import { SnackbarProvider } from "notistack";
 import ProcessPage from "./Pages/ProcessPage/ProcessPage";
@@ -37,17 +37,30 @@ function AppLayout() {
               </ProtectedRoute>
             }
           />
-          <Route path="/role-selection" element={<RoleSelection />} />
-          <Route path="/processes/new" element={
-            <ProtectedRoute>
-              <NewProcess />
-            </ProtectedRoute>
-          }/>
-          <Route path="/processes/:id" element={
-            <ProtectedRoute>
-              <ProcessPage />
-            </ProtectedRoute>
-          }/>
+          <Route
+            path="/role-selection"
+            element={
+              <ProtectedRoute>
+                <RoleSelection />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/processes/new"
+            element={
+              <ProtectedRoute>
+                <NewProcess />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/processes/:id"
+            element={
+              <ProtectedRoute>
+                <ProcessPage />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </div>
     </div>
@@ -57,9 +70,12 @@ function AppLayout() {
 export default function App() {
   return (
     <BrowserRouter>
-    <SnackbarProvider maxSnack={3} autoHideDuration={1500} anchorOrigin={{ vertical: "top", horizontal: "right" }}>
-      <AppLayout />
-    </SnackbarProvider>
+      <SnackbarProvider
+        maxSnack={3}
+        autoHideDuration={1500}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}>
+        <AppLayout />
+      </SnackbarProvider>
     </BrowserRouter>
   );
 }
